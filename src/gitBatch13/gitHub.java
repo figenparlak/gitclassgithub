05248e2ff7c05e8a5a85e56002464d274e6c66ec
figenparlak <figen.ayse.parlak@gmail.com>
2022-06-02 01:43:13 +0300

    2nd commit

--- src/gitBatch13/gitHub.java
+++ src/gitBatch13/gitHub.java
@@ -1,9 +1,22 @@
 package gitBatch13;
 
+import java.util.Scanner;
+
 public class gitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		  Scanner scan=new Scanner(System.in);
+		    int[] nums=new int[5];
+		    for(int i=0;i<nums.length;i++){
+		      nums[i]=scan.nextInt();
+		        
+		      
+
+
+		        } 
+		    
+		    
 
 	}
 
